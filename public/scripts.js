9e779affa5c63285775de7490ec3bec87baffb0a
Justin Mobley <justin.mobley89@outlook.com>
2020-07-23 08:22:09 +0000

    Testing really basic things

--- public/scripts.js
+++ public/scripts.js
@@ -6,17 +6,20 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+var geometry = new THREE.BoxGeometry( 2, 1, 1 );
 var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+var cube1 = new THREE.Mesh( geometry, material );
+var cube2 = new THREE.Mesh( geometry, material );
 
+scene.add( cube1 );
+scene.add( cube2 );
 camera.position.z = 5;
 
 var animate = function () {
 	requestAnimationFrame( animate );
 
-	cube.rotation.x += 0.1;
-	cube.rotation.y += 0.1;
+	cube1.rotation.x += 0.1;
+	cube1.rotation.y += 0.1;
 
 	renderer.render(scene, camera);
 };
